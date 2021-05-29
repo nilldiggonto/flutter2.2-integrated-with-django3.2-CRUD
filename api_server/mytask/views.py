9b48806f0090ea470fa51083589cb58b36ad0268
nilldiggonto <nilldiggonto@gmail.com>
2021-05-30 00:34:37 +0600

    Server Created

--- api_server/mytask/views.py
+++ api_server/mytask/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 from .serializers import MyTaskSerializers
+from .models import MyTask
+# from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# from rest_framework.viewsets import ModelViewSet
 # Create your views here.
+
+class MyTaskView(ListCreateAPIView):
+    queryset = MyTask.objects.all()
+    serializer_class =MyTaskSerializers
+
+class MyTaskViewDetail(RetrieveUpdateDestroyAPIView):
+    queryset = MyTask.objects.all()
+    serializer_class =MyTaskSerializers
