2d368225938321c3dbbc66888af51946e5520ee7
nilldiggonto <nilldiggonto@gmail.com>
2021-05-30 00:59:24 +0600

    Basic routing

---
+++ flutter/my_task/lib/add_edit.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddEditScreen extends StatefulWidget {
+  static const routeName = '/add-edit';
+  @override
+  _AddEditScreenState createState() => _AddEditScreenState();
+}
+
+class _AddEditScreenState extends State<AddEditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- flutter/my_task/lib/main.dart
+++ flutter/my_task/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_task/add_edit.dart';
+import 'package:my_task/homeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('My Daily Task'),
-      ),
-    ));
+      // home: HomeScreen(),
+      routes: {
+        '/': (context) => HomeScreen(),
+        AddEditScreen.routeName: (context) => AddEditScreen(),
+      },
+      // home: Scaffold(
+      // appBar: AppBar(
+      //   title: Text('My Daily Task'),
+      // ),
+    );
   }
 }
